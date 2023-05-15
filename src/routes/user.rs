1aa46e67a4fc4c7a79ca9cae80f82bed3e2ea5d9
George Rapeanu <alexandrurapeanu@yahoo.com>
2023-05-15 11:31:58 +0300

    modifies routes to give httpresponse

--- src/routes/user.rs
+++ src/routes/user.rs
@@ -1,3 +1,4 @@
+use crate::applib::errors::AppError;
 use crate::db::model::user::{NewUser, User};
 use crate::db::DbPool;
 use crate::service::user_service;
@@ -8,44 +9,68 @@ use actix_web::HttpResponse;
 pub async fn get_all_users(
     pool: web::Data<DbPool>,
 ) -> Result<HttpResponse, impl actix_web::ResponseError> {
-    user_service::get_all_users(pool).await.map(|users| {
-        HttpResponse::Created()
+    let result: Result<Vec<User>, AppError> = user_service::get_all_users(pool).await;
+    if let Err(error) = result {
+        return Err(error);
+    } else {
+        let users: Vec<User> = result.unwrap();
+        return Ok(HttpResponse::Ok()
             .content_type(ContentType::json())
-            .body(serde_json::to_string(&users).unwrap())
-    })
+            .json(users));
+    }
 }
 
 pub async fn get_user(
     pool: web::Data<DbPool>,
     user_id: web::Path<u64>,
-) -> Result<User, impl actix_web::ResponseError> {
-    user_service::get_user_by_id(pool, *user_id).await
+) -> Result<HttpResponse, impl actix_web::ResponseError> {
+    let result: Result<User, AppError> = user_service::get_user_by_id(pool, *user_id).await;
+    if let Err(error) = result {
+        return Err(error);
+    } else {
+        let user: User = result.unwrap();
+        return Ok(HttpResponse::Ok()
+            .content_type(ContentType::json())
+            .json(user));
+    }
 }
 
 pub async fn delete_user(
     pool: web::Data<DbPool>,
     user_id: web::Path<u64>,
-) -> Result<String, impl actix_web::ResponseError> {
-    user_service::delete_user(pool, *user_id)
-        .await
-        .map(|_| String::from("User deleted"))
+) -> Result<HttpResponse, impl actix_web::ResponseError> {
+    let result: Result<(), AppError> = user_service::delete_user(pool, *user_id).await;
+
+    if let Err(error) = result {
+        return Err(error);
+    } else {
+        return Ok(HttpResponse::Ok().into());
+    }
 }
 
 pub async fn update_user(
     pool: web::Data<DbPool>,
     user_id: web::Path<u64>,
     user_json: web::Json<NewUser>,
-) -> Result<String, impl actix_web::ResponseError> {
-    user_service::update_user(pool, *user_id, user_json)
-        .await
-        .map(|_| String::from("User updated"))
+) -> Result<HttpResponse, impl actix_web::ResponseError> {
+    let result: Result<(), AppError> = user_service::update_user(pool, *user_id, user_json).await;
+
+    if let Err(error) = result {
+        return Err(error);
+    } else {
+        return Ok(HttpResponse::Ok().into());
+    }
 }
 
 pub async fn create_user(
     pool: web::Data<DbPool>,
     user_json: web::Json<NewUser>,
-) -> Result<String, impl actix_web::ResponseError> {
-    user_service::create_user(pool, user_json)
-        .await
-        .map(|_| String::from("User created"))
+) -> Result<HttpResponse, impl actix_web::ResponseError> {
+    let result: Result<(), AppError> = user_service::create_user(pool, user_json).await;
+
+    if let Err(error) = result {
+        return Err(error);
+    } else {
+        return Ok(HttpResponse::Ok().into());
+    }
 }
