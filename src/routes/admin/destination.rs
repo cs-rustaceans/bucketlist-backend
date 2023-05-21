4f8c7e67790c4aad4b41689c0204b3250b64dddc
George Rapeanu <alexandrurapeanu@yahoo.com>
2023-05-21 19:51:11 +0300

    Adds get all unreviewed admin endpoint(https://trello.com/c/O7uhdVJ2)

--- src/routes/admin/destination.rs
+++ src/routes/admin/destination.rs
@@ -23,6 +23,25 @@ async fn get_all_destinations(
   }
 }
 
+async fn get_all_unreviewed_destinations(
+  pool: web::Data<DbPool>,
+) -> Result<HttpResponse, impl actix_web::ResponseError> {
+  let result: Result<Vec<Destination>, AppError> =
+    destination_service::admin_get_all_unreviewed_destinations(pool).await;
+  if let Err(error) = result {
+    return Err(error);
+  } else {
+    let users: Vec<Destination> = result.unwrap();
+    return Ok(
+      HttpResponse::Ok()
+        .content_type(ContentType::json())
+        .json(users),
+    );
+  }
+}
+
 pub fn configure_routes(cfg: &mut web::ServiceConfig) {
-  cfg.service(web::resource("").route(web::get().to(get_all_destinations)));
+  cfg
+    .service(web::resource("").route(web::get().to(get_all_destinations)))
+    .service(web::resource("/unreviewed").route(web::get().to(get_all_unreviewed_destinations)));
 }
--- src/service/admin/destination_service.rs
+++ src/service/admin/destination_service.rs
@@ -27,3 +27,27 @@ pub async fn admin_get_all_destinations(
     Err(_) => Err(AppError::internal_server_error()),
   }
 }
+
+pub async fn admin_get_all_unreviewed_destinations(
+  db_pool: web::Data<DbPool>,
+) -> Result<Vec<Destination>, AppError> {
+  let result: Result<Result<Vec<Destination>, AppError>, _> = web::block(move || {
+    let mut db_connection;
+
+    if let Ok(connection) = db_pool.get() {
+      db_connection = connection;
+    } else {
+      return Err(AppError::internal_server_error());
+    }
+    destinations
+      .filter(isReviewed.eq(false))
+      .load(&mut db_connection)
+      .map_err(|_| AppError::internal_server_error())
+  })
+  .await;
+
+  match result {
+    Ok(inner_result) => inner_result,
+    Err(_) => Err(AppError::internal_server_error()),
+  }
+}
