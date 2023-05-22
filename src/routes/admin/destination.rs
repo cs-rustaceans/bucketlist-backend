651bca5ec935d83e63aaeac1b7d6dee5e43a0f69
George Rapeanu <alexandrurapeanu@yahoo.com>
2023-05-22 09:15:51 +0300

    Refactors destination admin routes to use ? operator

--- src/routes/admin/destination.rs
+++ src/routes/admin/destination.rs
@@ -8,100 +8,69 @@ use actix_web::http::header::ContentType;
 use actix_web::web;
 use actix_web::HttpResponse;
 
-async fn get_all_destinations(
-  pool: web::Data<DbPool>,
-) -> Result<HttpResponse, impl actix_web::ResponseError> {
-  let result: Result<Vec<Destination>, AppError> =
-    destination_service::admin_get_all_destinations(pool).await;
-  if let Err(error) = result {
-    return Err(error);
-  } else {
-    let destinations: Vec<Destination> = result.unwrap();
-    return Ok(
-      HttpResponse::Ok()
-        .content_type(ContentType::json())
-        .json(destinations),
-    );
-  }
+async fn get_all_destinations(pool: web::Data<DbPool>) -> Result<HttpResponse, AppError> {
+  let destinations: Vec<Destination> =
+    destination_service::admin_get_all_destinations(pool).await?;
+  return Ok(
+    HttpResponse::Ok()
+      .content_type(ContentType::json())
+      .json(destinations),
+  );
 }
 
 async fn get_all_unreviewed_destinations(
   pool: web::Data<DbPool>,
-) -> Result<HttpResponse, impl actix_web::ResponseError> {
-  let result: Result<Vec<Destination>, AppError> =
-    destination_service::admin_get_all_unreviewed_destinations(pool).await;
-  if let Err(error) = result {
-    return Err(error);
-  } else {
-    let destinations: Vec<Destination> = result.unwrap();
-    return Ok(
-      HttpResponse::Ok()
-        .content_type(ContentType::json())
-        .json(destinations),
-    );
-  }
+) -> Result<HttpResponse, AppError> {
+  let destinations: Vec<Destination> =
+    destination_service::admin_get_all_unreviewed_destinations(pool).await?;
+  return Ok(
+    HttpResponse::Ok()
+      .content_type(ContentType::json())
+      .json(destinations),
+  );
 }
 
 async fn get_destination_by_id(
   pool: web::Data<DbPool>,
   destination_id: web::Path<u64>,
-) -> Result<HttpResponse, impl actix_web::ResponseError> {
-  let result: Result<Destination, AppError> =
-    destination_service::admin_get_destination_by_id(pool, *destination_id).await;
-  if let Err(error) = result {
-    return Err(error);
-  } else {
-    let destination: Destination = result.unwrap();
-    return Ok(
-      HttpResponse::Ok()
-        .content_type(ContentType::json())
-        .json(destination),
-    );
-  }
+) -> Result<HttpResponse, AppError> {
+  let destination: Destination =
+    destination_service::admin_get_destination_by_id(pool, *destination_id).await?;
+  return Ok(
+    HttpResponse::Ok()
+      .content_type(ContentType::json())
+      .json(destination),
+  );
 }
 
 async fn create_destination(
   pool: web::Data<DbPool>,
   new_destination_json: web::Json<NewDestination>,
-) -> Result<HttpResponse, impl actix_web::ResponseError> {
-  let result: Result<(), AppError> =
-    destination_service::admin_create_destination(pool, new_destination_json).await;
-  if let Err(error) = result {
-    return Err(error);
-  } else {
-    return Ok(HttpResponse::Created().into());
-  }
+) -> Result<HttpResponse, AppError> {
+  destination_service::admin_create_destination(pool, new_destination_json).await?;
+  return Ok(HttpResponse::Created().into());
 }
 
 async fn update_destination_by_id(
   pool: web::Data<DbPool>,
   destination_id: web::Path<u64>,
   update_destination_json: web::Json<UpdateDestination>,
-) -> Result<HttpResponse, impl actix_web::ResponseError> {
-  let result: Result<(), AppError> = destination_service::admin_update_destination_by_id(
+) -> Result<HttpResponse, AppError> {
+  destination_service::admin_update_destination_by_id(
     pool,
     *destination_id,
     update_destination_json,
   )
-  .await;
-  if let Err(error) = result {
-    return Err(error);
-  } else {
-    return Ok(HttpResponse::Ok().into());
-  }
+  .await?;
+  return Ok(HttpResponse::Ok().into());
 }
 
 async fn delete_destination_by_id(
   pool: web::Data<DbPool>,
   destination_id: web::Path<u64>,
-) -> Result<HttpResponse, impl actix_web::ResponseError> {
-  let result: Result<(), AppError> =
-    destination_service::admin_delete_destination_by_id(pool, *destination_id).await;
-  if let Err(error) = result {
-    return Err(error);
-  } else {
-    return Ok(HttpResponse::Ok().into());
-  }
+) -> Result<HttpResponse, AppError> {
+  destination_service::admin_delete_destination_by_id(pool, *destination_id).await?;
+  return Ok(HttpResponse::Ok().into());
 }
 
 pub fn configure_routes(cfg: &mut web::ServiceConfig) {
