48eb5aace41f15d80095a54b5895e9e890ac7af8
George Rapeanu <alexandrurapeanu@yahoo.com>
2023-05-23 08:28:29 +0300

    Refactors to use ? more

--- src/service/admin/user_service.rs
+++ src/service/admin/user_service.rs
@@ -14,89 +14,65 @@ pub async fn admin_create_user(
 ) -> Result<(), AppError> {
   let mut new_user: NewUser = user_json.into_inner();
 
-  if let Ok(hashed_password) = hash(new_user.password, DEFAULT_COST) {
-    new_user.password = hashed_password;
-  } else {
-    return Err(AppError::internal_server_error());
-  }
+  new_user.password =
+    hash(new_user.password, DEFAULT_COST).map_err(|_| AppError::internal_server_error())?;
 
-  let result = web::block(move || {
-    let mut db_connection;
-
-    if let Ok(connection) = db_pool.get() {
-      db_connection = connection;
-    } else {
-      return Err(AppError::internal_server_error());
-    }
+  web::block(move || -> Result<(), AppError> {
+    let mut db_connection = db_pool
+      .get()
+      .map_err(|_| AppError::internal_server_error())?;
 
     diesel::insert_into(users::table)
       .values(&new_user)
       .execute(&mut db_connection)
       .map(|_| ())
-      .map_err(|_| AppError::internal_server_error())
+      .map_err(|_| AppError::internal_server_error())?;
+    Ok(())
   })
-  .await;
+  .await??;
 
-  match result {
-    Ok(inner_result) => inner_result,
-    Err(_) => Err(AppError::internal_server_error()),
-  }
+  Ok(())
 }
 
 pub async fn admin_get_all_users(db_pool: web::Data<DbPool>) -> Result<Vec<GetUserDTO>, AppError> {
-  let result: Result<Result<Vec<User>, AppError>, _> = web::block(move || {
-    let mut db_connection;
-
-    if let Ok(connection) = db_pool.get() {
-      db_connection = connection;
-    } else {
-      return Err(AppError::internal_server_error());
-    }
-    users
-      .load(&mut db_connection)
-      .map_err(|_| AppError::internal_server_error())
-  })
-  .await;
-
-  match result {
-    Ok(inner_result) => inner_result.map(|result_users| {
-      result_users
-        .into_iter()
-        .map(|user| GetUserDTO::from(user))
-        .collect()
-    }),
-    Err(_) => Err(AppError::internal_server_error()),
-  }
+  Ok(
+    web::block(move || -> Result<Vec<User>, AppError> {
+      let mut db_connection = db_pool
+        .get()
+        .map_err(|_| AppError::internal_server_error())?;
+      users
+        .load::<User>(&mut db_connection)
+        .map_err(|_| AppError::internal_server_error())
+    })
+    .await??
+    .into_iter()
+    .map(|user| GetUserDTO::from(user))
+    .collect(),
+  )
 }
 
 pub async fn admin_get_user_by_id(
   db_pool: web::Data<DbPool>,
   user_id: u64,
 ) -> Result<GetUserDTO, AppError> {
-  let result: Result<Result<Vec<User>, AppError>, _> = web::block(move || {
-    let mut db_connection;
-
-    if let Ok(connection) = db_pool.get() {
-      db_connection = connection;
-    } else {
-      return Err(AppError::internal_server_error());
-    }
-    users
-      .filter(id.eq(user_id))
-      .limit(1)
-      .load::<User>(&mut db_connection)
-      .map_err(|_| AppError::internal_server_error())
-  })
-  .await;
-
-  match result {
-    Ok(Ok(mut result_users)) => result_users.pop().map_or(
+  Ok(
+    web::block(move || -> Result<Vec<User>, AppError> {
+      let mut db_connection = db_pool
+        .get()
+        .map_err(|_| AppError::internal_server_error())?;
+      users
+        .filter(id.eq(user_id))
+        .limit(1)
+        .load::<User>(&mut db_connection)
+        .map_err(|_| AppError::internal_server_error())
+    })
+    .await??
+    .pop()
+    .map_or(
       Err(AppError::not_found(Some(String::from("user")))),
       |user| Ok(GetUserDTO::from(user)),
-    ),
-    Ok(Err(inner_error)) => Err(inner_error),
-    Err(_) => Err(AppError::internal_server_error()),
-  }
+    )?,
+  )
 }
 
 pub async fn admin_update_user(
@@ -107,21 +83,14 @@ pub async fn admin_update_user(
   let mut update_user: UpdateUser = user_json.into_inner();
 
   if let Some(plain_text_password) = update_user.password {
-    if let Ok(hashed_password) = hash(plain_text_password, DEFAULT_COST) {
-      update_user.password = Some(hashed_password);
-    } else {
-      return Err(AppError::internal_server_error());
-    }
+    update_user.password =
+      Some(hash(plain_text_password, DEFAULT_COST).map_err(|_| AppError::internal_server_error())?);
   }
 
-  let result = web::block(move || {
-    let mut db_connection;
-
-    if let Ok(connection) = db_pool.get() {
-      db_connection = connection;
-    } else {
-      return Err(AppError::internal_server_error());
-    }
+  web::block(move || {
+    let mut db_connection = db_pool
+      .get()
+      .map_err(|_| AppError::internal_server_error())?;
 
     diesel::update(users.filter(id.eq(user_id)))
       .set(update_user)
@@ -129,33 +98,23 @@ pub async fn admin_update_user(
       .map(|_| ())
       .map_err(|_| AppError::internal_server_error())
   })
-  .await;
+  .await??;
 
-  match result {
-    Ok(inner_result) => inner_result,
-    Err(_) => Err(AppError::internal_server_error()),
-  }
+  Ok(())
 }
 
 pub async fn admin_delete_user(db_pool: web::Data<DbPool>, user_id: u64) -> Result<(), AppError> {
-  let result = web::block(move || {
-    let mut db_connection;
-
-    if let Ok(connection) = db_pool.get() {
-      db_connection = connection;
-    } else {
-      return Err(AppError::internal_server_error());
-    }
+  web::block(move || {
+    let mut db_connection = db_pool
+      .get()
+      .map_err(|_| AppError::internal_server_error())?;
 
     diesel::delete(users.filter(id.eq(user_id)))
       .execute(&mut db_connection)
       .map(|_| ())
       .map_err(|_| AppError::internal_server_error())
   })
-  .await;
+  .await??;
 
-  match result {
-    Ok(inner_result) => inner_result,
-    Err(_) => Err(AppError::internal_server_error()),
-  }
+  Ok(())
 }
