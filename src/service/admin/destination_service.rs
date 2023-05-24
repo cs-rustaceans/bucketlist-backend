ec4629435d7680fb34f16d4ab29652936951d6f3
George Rapeanu <alexandrurapeanu@yahoo.com>
2023-05-24 18:46:28 +0300

    addresses https://trello.com/c/GnUsfp8k:wq

--- src/service/admin/destination_service.rs
+++ src/service/admin/destination_service.rs
@@ -91,7 +91,7 @@ pub async fn admin_update_destination_by_id(
 ) -> Result<(), AppError> {
   let update_destination: UpdateDestination = update_destination_json.into_inner();
 
-  web::block(move || {
+  let row_count: usize = web::block(move || {
     let mut db_connection = db_pool
       .get()
       .map_err(|_| AppError::internal_server_error())?;
@@ -99,28 +99,35 @@ pub async fn admin_update_destination_by_id(
     diesel::update(destinations.filter(id.eq(destination_id)))
       .set(update_destination)
       .execute(&mut db_connection)
-      .map(|_| ())
       .map_err(|_| AppError::internal_server_error())
   })
   .await??;
 
-  Ok(())
+  if row_count == 0 {
+    Err(AppError::not_found(Some(String::from("destination"))))
+  } else {
+    Ok(())
+  }
 }
 
 pub async fn admin_delete_destination_by_id(
   db_pool: web::Data<DbPool>,
   destination_id: u64,
 ) -> Result<(), AppError> {
-  web::block(move || {
+  let row_count: usize = web::block(move || {
     let mut db_connection = db_pool
       .get()
       .map_err(|_| AppError::internal_server_error())?;
 
     diesel::delete(destinations.filter(id.eq(destination_id)))
       .execute(&mut db_connection)
-      .map(|_| ())
       .map_err(|_| AppError::internal_server_error())
   })
   .await??;
-  Ok(())
+
+  if row_count == 0 {
+    Err(AppError::not_found(Some(String::from("destination"))))
+  } else {
+    Ok(())
+  }
 }
