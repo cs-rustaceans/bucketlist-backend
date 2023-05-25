2683b4a07e469c57c66b6cb93558d8c99608179f
George Rapeanu <alexandrurapeanu@yahoo.com>
2023-05-25 11:25:55 +0300

    Adds employee change password

--- src/dto/mod.rs
+++ src/dto/mod.rs
@@ -1,3 +1,4 @@
 pub mod get_user_dto;
 pub mod login_form;
 pub mod login_token_claims;
+pub mod user_change_password_dto;
--- src/routes/employee/user.rs
+++ src/routes/employee/user.rs
@@ -1,6 +1,22 @@
+use crate::applib::errors::AppError;
+use crate::db::model::user::User;
+use crate::db::DbPool;
+use crate::dto::user_change_password_dto::UserChangePasswordDTO;
 use crate::routes::common;
-use actix_web::web;
+use crate::service::employee::user_service;
+use actix_web::{web, HttpResponse};
+
+async fn employee_change_password(
+  db_pool: web::Data<DbPool>,
+  user: User,
+  change_password_json: web::Json<UserChangePasswordDTO>,
+) -> Result<HttpResponse, AppError> {
+  user_service::employee_change_password(db_pool, user, change_password_json).await?;
+  Ok(HttpResponse::Ok().into())
+}
 
 pub fn configure_routes(cfg: &mut web::ServiceConfig) {
-  cfg.service(web::resource("/me").route(web::get().to(common::user::get_user)));
+  cfg
+    .service(web::resource("/me").route(web::get().to(common::user::get_user)))
+    .service(web::resource("/change-password").route(web::post().to(employee_change_password)));
 }
