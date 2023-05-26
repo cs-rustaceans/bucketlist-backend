670fe6f4c4e0af96a18e6d977512c42d17e0377d
George Rapeanu <alexandrurapeanu@yahoo.com>
2023-05-26 18:31:11 +0300

    Adds enpoint for admin to fetch favorite destinations

--- src/routes/admin/mod.rs
+++ src/routes/admin/mod.rs
@@ -1,3 +1,4 @@
+pub mod bucketlist_item;
 pub mod destination;
 pub mod user;
 use crate::guard::admin::admin_guard;
@@ -8,6 +9,7 @@ pub fn configure_routes(cfg: &mut web::ServiceConfig) {
     web::scope("")
       .guard(actix_web::guard::fn_guard(admin_guard))
       .service(web::scope("/users").configure(user::configure_routes))
-      .service(web::scope("/destinations").configure(destination::configure_routes)),
+      .service(web::scope("/destinations").configure(destination::configure_routes))
+      .service(web::scope("/bucketlist-items").configure(bucketlist_item::configure_routes)),
   );
 }
--- src/service/admin/mod.rs
+++ src/service/admin/mod.rs
@@ -1,2 +1,3 @@
+pub mod bucketlist_service;
 pub mod destination_service;
 pub mod user_service;
